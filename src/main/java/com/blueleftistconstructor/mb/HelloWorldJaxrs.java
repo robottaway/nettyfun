2b0996de9b60b07d7788797f11a0a6677dcf4455
Robert Ottaway <robottaway@gmail.com>
2013-02-24 07:34:32 -0800

    working on browser side

--- src/main/java/com/blueleftistconstructor/mb/HelloWorldJaxrs.java
+++ src/main/java/com/blueleftistconstructor/mb/HelloWorldJaxrs.java
@@ -1,10 +1,9 @@
 package com.blueleftistconstructor.mb;
 
-import javax.ws.rs.GET;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.OPTIONS;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
 import javax.ws.rs.core.Cookie;
 import javax.ws.rs.core.NewCookie;
 import javax.ws.rs.core.Response;
@@ -23,21 +22,33 @@ public class HelloWorldJaxrs
 		return Response.ok().header("Access-Control-Allow-Origin", "*").build();
 	}
 	
-	@GET
-	@Produces("text/plain")
-	public String details() 
-	{
-		return "Hello World!";
-	}
-	
+	/**
+	 * Log user in via AJAX auth, add cookie for user account.
+	 */
 	@POST
-	@Produces("text/plain")
 	public Response authenticate() 
 	{
 		Cookie c1 = new Cookie("user", "rob", "/", ".blueleftistconstructor.com");
 		NewCookie nc1 = new NewCookie(c1, null, 3600, false);
-		return Response.ok("Hello World!")
-				.header("Access-Control-Allow-Origin", "http://farm.blueleftistconstructor.com:8082")
+		return Response.noContent()
+				.header("Access-Control-Allow-Origin", "http://mb.blueleftistconstructor.com:8082")
+				.header("Access-Control-Allow-Credentials", "true")
+				.cookie(nc1)
+				.build();
+	}
+	
+	/**
+	 * Log user out of application.
+	 * 
+	 * Make sure browser wipes the cookie.
+	 */
+	@DELETE
+	public Response deauthenticate() 
+	{
+		Cookie c1 = new Cookie("user", "", "/", ".blueleftistconstructor.com");
+		NewCookie nc1 = new NewCookie(c1, null, 0, false);
+		return Response.noContent()
+				.header("Access-Control-Allow-Origin", "http://mb.blueleftistconstructor.com:8082")
 				.header("Access-Control-Allow-Credentials", "true")
 				.cookie(nc1)
 				.build();
